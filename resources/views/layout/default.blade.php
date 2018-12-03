17a0c5b059c6777cc18922c57d806a3b963d186b
yanhk <705177961@qq.com>
2018-12-03 22:44:28 +0800

    构建页面

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>@yield('title', 'Laravel.1')- Yanhk</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.default')
+@section('content')
+    <h1>首页</h1>
+@stop
