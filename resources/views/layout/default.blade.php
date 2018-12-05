1352331eb118506b102de5afd58f9646e6e3e733
yanhk <705177961@qq.com>
2018-12-05 22:05:27 +0800

    优化样式

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -2,8 +2,16 @@
 <html>
 <head>
     <title>@yield('title', 'Laravel.1')- Yanhk</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+    @include('layout._header')
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layout._footer')
+        </div>
+
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layout.default')
 @section('content')
-    <h1>首页</h1>
+    <div class="jumbotron">
+        <h1>HELLO WORD</h1>
+        <p class="lead">
+            你现在看到的是o(╯□╰)o  lv1.1
+        </p>
+        <p>
+            一切即将开始
+        </p>
+        <p>
+            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
+
 @stop
