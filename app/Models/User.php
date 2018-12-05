1b3706987ca9aa5670a5c8d750e22e0fcd560f5d
yanhk <705177961@qq.com>
2018-12-05 22:29:55 +0800

    用户模型保存

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,9 +9,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    //修改对应数据表
+    protected $tables = 'users';
     /**
      * The attributes that are mass assignable.
-     *
+     *  只有包含到该属性中的字段才能被正常更新
      * @var array
      */
     protected $fillable = [
@@ -20,7 +22,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 在用户实例中被隐藏
      * @var array
      */
     protected $hidden = [
